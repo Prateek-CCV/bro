f67b4d990522800c8d76ca84f3c20d486a643b0a
Prateek Raj <prateekraj807@gmail.com>
2021-12-03 10:39:24 +0530

    second commit

--- connect.php
+++ connect.php
@@ -1,8 +1,8 @@
 <?php
-$hostname="aa4y9og0dyqb68.c2u0hj6ejqek.us-east-1.rds.amazonaws.com"; //local server name default localhost
+$hostname="aa1c9zufc36r35e.c2u0hj6ejqek.us-east-1.rds.amazonaws.com"; //local server name default localhost
 $username="prateek";  //mysql username default is root.
 $password="prateek123";       //blank if no password is set for mysql.
-$database="aa4y9og0dyqb68";  //database name which you created
+$database="aa1c9zufc36r35e";  //database name which you created
 $con=mysql_connect($hostname,$username,$password);
 if(! $con)
 {

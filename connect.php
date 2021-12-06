6a38ebf0b857280b2b20683e424f7274b0ae7d21
Prateek-CCV <60535464+Prateek-CCV@users.noreply.github.com>
2021-12-06 16:49:06 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,8 +1,8 @@
 <?php
-$hostname="aa1c9zufc36r35e.c2u0hj6ejqek.us-east-1.rds.amazonaws.com"; //local server name default localhost
+$hostname="aa15jyn89zhnxbt.c2u0hj6ejqek.us-east-1.rds.amazonaws.com"; //local server name default localhost
 $username="prateek";  //mysql username default is root.
 $password="prateek123";       //blank if no password is set for mysql.
-$database="aa1c9zufc36r35e";  //database name which you created
+$database="aa15jyn89zhnxbt";  //database name which you created
 $con=mysql_connect($hostname,$username,$password);
 if(! $con)
 {
